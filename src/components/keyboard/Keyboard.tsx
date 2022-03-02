271ae3b7c7cb5131582cdfac82700fbea98f1282
Toph Bantoto <lcbantoto@gmail.com>
2022-03-02 15:04:54 -0500

    Swap Enter and Delete keys

--- src/components/keyboard/Keyboard.tsx
+++ src/components/keyboard/Keyboard.tsx
@@ -134,11 +134,11 @@ export const Keyboard = ({
               isRevealing={isRevealing}
             />
           ))}
-          <Key value="DELETE" onClick={onClick}>
-            <BackspaceIcon className="h-6 w-6 mr-2 cursor-pointer dark:stroke-white" />
-          </Key>
         </div>
         <div className="flex justify-center w-screen">
+          <Key width={65.4} value="DELETE" onClick={onClick}>
+            <BackspaceIcon className="h-6 w-6 mr-2 cursor-pointer dark:stroke-white" />
+          </Key>
           {['Z', 'X', 'C', 'V', 'B', 'N', 'M'].map((key) => (
             <Key
               value={key}
@@ -148,7 +148,7 @@ export const Keyboard = ({
               isRevealing={isRevealing}
             />
           ))}
-          <Key width={128} value="ENTER" onClick={onClick}>
+          <Key width={65.4} value="ENTER" onClick={onClick}>
             {ENTER}
           </Key>
         </div>
