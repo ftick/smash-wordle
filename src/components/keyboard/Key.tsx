fd6db02cc1753f909f53a5d902fa341fbe3c3408
Toph Bantoto <lcbantoto@gmail.com>
2022-03-02 13:04:23 -0500

    Edit look of absent key

--- src/components/keyboard/Key.tsx
+++ src/components/keyboard/Key.tsx
@@ -25,19 +25,19 @@ export const Key = ({
   const isHighContrast = getStoredIsHighContrastMode()
 
   const classes = classnames(
-    'flex items-center justify-center rounded mx-0.5 text-xs font-bold cursor-pointer select-none dark:text-white',
+    'flex items-center justify-center rounded mx-0.5 text-xs font-bold cursor-pointer select-none',
     {
       'transition ease-in-out': isRevealing,
-      'bg-slate-200 dark:bg-slate-600 hover:bg-slate-300 active:bg-slate-400':
+      'bg-slate-200 dark:bg-slate-600 hover:bg-slate-300 active:bg-slate-400 dark:text-white':
         !status,
-      'bg-slate-400 dark:bg-slate-800 text-white': status === 'absent',
-      'bg-orange-500 hover:bg-orange-600 active:bg-orange-700 text-white':
+      'bg-slate-400 dark:bg-slate-800 text-slate-600 dark:text-slate-600': status === 'absent',
+      'bg-orange-500 hover:bg-orange-600 active:bg-orange-700 dark:text-white':
         status === 'correct' && isHighContrast,
-      'bg-cyan-500 hover:bg-cyan-600 active:bg-cyan-700 text-white':
+      'bg-cyan-500 hover:bg-cyan-600 active:bg-cyan-700 dark:text-white':
         status === 'present' && isHighContrast,
-      'bg-green-500 hover:bg-green-600 active:bg-green-700 text-white':
+      'bg-green-500 hover:bg-green-600 active:bg-green-700 dark:text-white':
         status === 'correct' && !isHighContrast,
-      'bg-yellow-500 hover:bg-yellow-600 active:bg-yellow-700 text-white':
+      'bg-yellow-500 hover:bg-yellow-600 active:bg-yellow-700 dark:text-white':
         status === 'present' && !isHighContrast,
     }
   )
